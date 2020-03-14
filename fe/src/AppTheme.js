b7ab8759c8fe0ae61d22f04e8dc9e797ecdb796f
Tiago Fragoso <tiagofragoso@outlook.com>
2020-03-14 15:35:52 +0000

    Add react-hook-form example

--- fe/src/AppTheme.js
+++ fe/src/AppTheme.js
@@ -1,11 +1,10 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import purple from "@material-ui/core/colors/purple";
-import green from "@material-ui/core/colors/green";
+import { blue, purple } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
     palette: {
-        primary: purple,
-        secondary: green,
+        primary: blue,
+        secondary: purple,
     },
     status: {
         danger: "orange",
--- fe/src/pages/Homepage.js
+++ fe/src/pages/Homepage.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { TextField } from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import { Button, TextField } from "@material-ui/core";
 
 
-const HomePage = () => (
-    <div>
-        <TextField placeholder="Search"/>
-    </div>
-);
+const HomePage = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const submitSearch = (data) => console.log(data);
+
+    return (
+        <div>
+            <form noValidate autoComplete="off" onSubmit={handleSubmit(submitSearch)}>
+                <TextField inputRef={register} name="q" label="Search" />
+                <Button variant="contained" color="primary" type="submit">Search</Button>
+            </form>
+        </div>
+    );
+
+};
 
 export default HomePage;
