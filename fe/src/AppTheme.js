0af3f7454223c5b5d2020f8f6954345bb6bc5e28
Tiago Fragoso <tiagofragoso@outlook.com>
2020-04-30 16:17:22 +0100

    Add Rubik&Karla fonts

--- fe/src/AppTheme.js
+++ fe/src/AppTheme.js
@@ -1,7 +1,32 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import { blue, purple } from "@material-ui/core/colors";
 
+const bodyFontFamily = ["\"Rubik\"", "Roboto", "\"Helvetica\"", "Arial", "sans-serif"];
+const headerFontFamily = ["\"Karla\"", ...bodyFontFamily];
+
 const theme = createMuiTheme({
+    typography: {
+        fontFamily: bodyFontFamily.join(","),
+        h1: {
+            fontFamily: headerFontFamily.join(","),
+        },
+        h2: {
+            fontFamily: headerFontFamily.join(","),
+        },
+        h3: {
+            fontFamily: headerFontFamily.join(","),
+            fontWeight: 700,
+        },
+        h4: {
+            fontFamily: headerFontFamily.join(","),
+        },
+        h5: {
+            fontFamily: headerFontFamily.join(","),
+        },
+        h6: {
+            fontFamily: headerFontFamily.join(","),
+        },
+    },
     palette: {
         primary: blue,
         secondary: purple,
@@ -11,4 +36,4 @@ const theme = createMuiTheme({
     },
 });
 
-export default theme;
+export default responsiveFontSizes(theme);
